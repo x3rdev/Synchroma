8a6079f1c05e385fa08be2cf53246a761aa18625
X3RMINATION <nicolaeungur@gmail.com>
2023-09-03 11:35:41 -0700

    fixed centrifuge item rendering

--- src/main/java/com/github/x3r/synchroma/client/renderer/block/CentrifugeRenderer.java
+++ src/main/java/com/github/x3r/synchroma/client/renderer/block/CentrifugeRenderer.java
@@ -47,10 +47,11 @@ public class CentrifugeRenderer extends ControllerRenderer<CentrifugeBlockEntity
         ItemStack stack = animatable.getItem(0);
         if(!stack.isEmpty()) {
             float rot = animatable.boneSnapshots.get("rotating_part").getRotY();
-            Vec3 v = new Vec3(0.75 * animatable.getSpeedRatio(), 1, 0).yRot(rot).add(new Vec3(0.5, 0, 0.5).yRot((float) Math.toRadians(180-animatable.getBlockState().getValue(ControllerBlock.FACING).toYRot())));
-            animatable.getBlockPos();
+            Vec3 v = new Vec3(0.75 * animatable.getSpeedRatio(), 1, 0).yRot(rot);
             ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
             poseStack.pushPose();
+            poseStack.translate(0.5, 0, 0.5);
+            poseStack.mulPose(Axis.YP.rotation((float) Math.toRadians(180 - getFacing(animatable).toYRot())));
             poseStack.translate(v.x, v.y, v.z);
             itemRenderer.renderStatic(stack, ItemDisplayContext.GROUND, packedLight, OverlayTexture.NO_OVERLAY, poseStack, bufferSource, animatable.getLevel(), animatable.hashCode());
             poseStack.popPose();
